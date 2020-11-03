3a6fbdd10dc9fe8969d62382170e2d7271261e40
Cindy Okino <cindy.okino@gmail.com>
2020-11-02 22:20:23 -0500

    commit before pull master

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -21,11 +21,21 @@ class LeadsController < ApplicationController
                       :message,
                       :attached_file )  
                       
+        
+        full_name = params[:full_name]
+        email = params[:email]
+        project_name = params[:project_name]
+
         from = Email.new(email: 'cindy-okino@hotmail.com')
         to = Email.new(email: 'cindy.okino@gmail.com')
+
+        template_id = "d-c6ab731e2c5249cf8f7405d6cf96fbfe"
         subject = 'Sending with SendGrid is Fun'
         content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+
         mail = Mail.new(from, subject, to, content)
+        # mail = Mail.new(from, to, full_name, project_name)
+
         
         sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
         response = sg.client.mail._('send').post(request_body: mail.to_json)
