8d879bc5f2188c58c8e4698afb7b47f686e460de
tonTed <yddetco@gmail.com>
2020-11-02 19:29:35 -0500

    zendesk works with leads

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,21 +1,23 @@
 class LeadsController < ApplicationController
-    # skip_before_action :verify_authenticity_token 
-    def new  
-      @lead = Lead.new     
-    end
-    def create        
-      @lead = Lead.new(lead_params)   
-      @lead.save!     
-    end
-    def lead_params        
-      params.permit(  :full_name,
-                      :company_name,
-                      :email,
-                      :phone_number,
-                      :project_name,
-                      :project_description,
-                      :department,
-                      :message,
-                      :attached_file )     
-    end
+  def new  
+    @lead = Lead.new     
+  end
+
+  def create        
+    @lead = Lead.new(lead_params)   
+    @lead.save!  
+    helpers.ticket(lead_params)
+  end
+  
+  def lead_params        
+    params.permit(  :full_name,
+      :company_name,
+      :email,
+      :phone_number,
+      :project_name,
+      :project_description,
+      :department,
+      :message,
+      :attached_file )     
+  end
 end
