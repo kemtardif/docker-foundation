fc48c85454230aead9fa5dd4646a9bc5d8777ff7
Cindy Okino <cindy.okino@gmail.com>
2020-11-06 15:22:41 -0500

    Test ssh

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.14.1"
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "CindyOkino"
-set :repo_url, "https://github.com/cindyokino/Rocket_Elevators_API"
+set :repo_url, "git@github.com:cindyokino/Rocket_Elevators_API.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
