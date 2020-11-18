4197ee104f67bb1c8d224d0938b4d19d0a1854bd
Kem Tardif <tardif.kremlin@gmail.com>
2020-11-18 14:04:38 -0500

    hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     helper :all
-    skip_before_filter :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
     protect_from_forgery prepend: true, with: :exception
 
     protected
