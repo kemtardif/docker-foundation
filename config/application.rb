896225e0898166d26f542f7dc9d13dd1ed7a9801
Kem Tardif <tardif.kremlin@gmail.com>
2020-11-18 17:49:51 -0500

    fdf

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Rocket_Elevators_Information_System
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-  config.session_store :cookie_store
+    config.session_store :cookie_store
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
     config.middleware.use Rack::MethodOverride
