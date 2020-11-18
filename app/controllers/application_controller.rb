d2a58b780820dcc3726197b1242b46cb4a4cc140
Kem Tardif <tardif.kremlin@gmail.com>
2020-11-18 13:56:26 -0500

    rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
     helper :all
-    skip_before_action :verify_authenticity_token
     protect_from_forgery prepend: true, with: :exception
+    skip_before_action :verify_authenticity_token
 
     protected
     #Jorge - redirecto to home after signin or signup
